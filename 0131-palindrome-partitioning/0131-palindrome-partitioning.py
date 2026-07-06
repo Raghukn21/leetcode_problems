507c6545eb8cb217f18fa169dc8ad6b544a1c472
Raghukn21 <raghukn.2003@gmail.com>
2026-07-06 20:01:27 +0530

    Time: 39 ms (80.09%), Space: 33.7 MB (61.02%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,28 +1,26 @@
 class Solution:
     def partition(self, s: str) -> list[list[str]]:
-        result = []
-        n = len(s)
-
-        def is_palindrome(left, right):
-            while left < right:
-                if s[left] != s[right]:
-                    return False
-                left += 1
-                right -= 1
-            return True
-
-        def backtrack(start, current_partition):
-            # Base case: we've reached the end of the string,
-            # meaning current_partition is a complete valid partitioning
-            if start == n:
-                result.append(current_partition[:])
+        res = []
+        
+        def is_palindrome(sub: str) -> bool:
+            return sub == sub[::-1]
+            
+        def backtrack(start: int, path: list[str]):
+            # Base case: if we've reached the end of the string
+            if start == len(s):
+                res.append(list(path))
                 return
-
-            for end in range(start, n):
-                if is_palindrome(start, end):
-                    current_partition.append(s[start:end + 1])
-                    backtrack(end + 1, current_partition)
-                    current_partition.pop()  # backtrack: undo this choice
-
+            
+            # Explore all possible substrings starting from 'start'
+            for end in range(start + 1, len(s) + 1):
+                substring = s[start:end]
+                if is_palindrome(substring):
+                    # Choose
+                    path.append(substring)
+                    # Explore
+                    backtrack(end, path)
+                    # Un-choose (backtrack)
+                    path.pop()
+                    
         backtrack(0, [])
-        return result
+        return res
