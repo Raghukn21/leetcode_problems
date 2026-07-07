e6feaaa4a0ced7fe4c85d7c55b4b4bacb6834d2e
Raghukn21 <raghukn.2003@gmail.com>
2026-07-07 11:56:57 +0530

    Time: 146 ms (26.19%), Space: 77.3 MB (33.74%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.py
@@ -0,0 +1,55 @@
+class Node:
+    def __init__(self, key=0, value=0):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}  # key -> Node
+        
+        # Dummy head and tail sentinels
+        self.head = Node()
+        self.tail = Node()
+        self.head.next = self.tail
+        self.tail.prev = self.head
+    
+    def _remove(self, node):
+        """Detach node from the linked list."""
+        node.prev.next = node.next
+        node.next.prev = node.prev
+    
+    def _add_to_front(self, node):
+        """Insert node right after head (most recently used position)."""
+        node.next = self.head.next
+        node.prev = self.head
+        self.head.next.prev = node
+        self.head.next = node
+    
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        
+        node = self.cache[key]
+        self._remove(node)
+        self._add_to_front(node)
+        return node.value
+    
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            node = self.cache[key]
+            node.value = value
+            self._remove(node)
+            self._add_to_front(node)
+        else:
+            if len(self.cache) >= self.capacity:
+                # Evict least recently used (node just before tail)
+                lru = self.tail.prev
+                self._remove(lru)
+                del self.cache[lru.key]
+            
+            new_node = Node(key, value)
+            self.cache[key] = new_node
+            self._add_to_front(new_node)
